9947f0db35d3a4a1a907267ca149f858ff0d7f35
Michal Murray <michal.murray@britishcouncil.org>
2025-04-14 17:22:17 +0200

    feat: change project layout, add db connection

--- service/ClientService.py
+++ service/ClientService.py
@@ -7,6 +7,7 @@ class ClientService(BaseService):
 
     def get_client(self, id):
         query = f"SELECT * FROM clients WHERE id = {id}"
+        # todo: return Client.py model client
         return super().db.execute_read_query(query)
 
     def add_client(self):
