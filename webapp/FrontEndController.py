b35ace5a4ecb49c7750b8304d49f384b5d11ff58
Artur <archi.tibili@gmail.com>
2025-07-03 17:36:39 +0200

    Small change

--- webapp/FrontEndController.py
+++ webapp/FrontEndController.py
@@ -1,3 +1,4 @@
+from numbers import Number
 from flask import Flask, render_template, request, url_for, jsonify
 import requests
 import logging
@@ -127,7 +128,7 @@ def own_transfer():
             if i["id"] == accepted_data["source_account_id"]:
                 print(i['balance'])
                 print(accepted_data["value"])
-                if i["balance"] < accepted_data["value"]:
+                if Number(i["balance"]) < Number(accepted_data["value"]):
                     print("hej")
                     raise Exception("Not enough funds")
         print(json.dumps(accepted_data))
